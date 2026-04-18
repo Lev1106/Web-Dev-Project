6d1d761480debc080bb5ddead74ec9e6ba2598e7
Olzhas <o_kuzembayev@kbtu.kz>
2026-04-18 23:23:39 +0500

    profile components updated to changable

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MealService } from '../../services/meal.service';
@@ -13,45 +13,12 @@ import { MealService } from '../../services/meal.service';
 export class ProfileComponent {
   mealService = inject(MealService);
 
-  isEditing = signal(false);
-
-  name = signal('Alex Johnson');
-  weight = signal(72);
-  height = signal(178);
-  age = signal(28);
-  activityLevel = signal('Moderate');
-
-  // для редактирования
-  tempName = '';
-  tempWeight = 0;
-  tempHeight = 0;
-  tempAge = 0;
-  tempActivity = '';
-
-  startEdit() {
-    this.tempName = this.name();
-    this.tempWeight = this.weight();
-    this.tempHeight = this.height();
-    this.tempAge = this.age();
-    this.tempActivity = this.activityLevel();
-    this.isEditing.set(true);
-  }
-
-  saveEdit() {
-    this.name.set(this.tempName);
-    this.weight.set(this.tempWeight);
-    this.height.set(this.tempHeight);
-    this.age.set(this.tempAge);
-    this.activityLevel.set(this.tempActivity);
-    this.isEditing.set(false);
-  }
-
-  cancelEdit() {
-    this.isEditing.set(false);
-  }
+  profile = JSON.parse(localStorage.getItem('user_profile') || '{}');
 
   getInitials(): string {
-    return this.name().split(' ').map(n => n[0]).join('').toUpperCase();
+    const f = this.profile.firstName?.[0] || '';
+    const l = this.profile.lastName?.[0] || '';
+    return (f + l).toUpperCase();
   }
 
   onGoalChange(value: string) {
