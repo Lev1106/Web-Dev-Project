4b729135a5623bd89d87ac123ecec38bf184b29b
Olzhas <o_kuzembayev@kbtu.kz>
2026-04-18 23:30:11 +0500

    добавил log out

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  logout() {
+    this.auth.logout();
+  }
+
+  isLoggedIn() {
+    return this.auth.isLoggedIn();
+  }
+}
