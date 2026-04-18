b9babf636b2eee73d023060cfd701769ec434c68
Olzhas <o_kuzembayev@kbtu.kz>
2026-04-18 23:10:19 +0500

    update routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HistoryComponent } from './components/history/history.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
+import { SetupComponent } from './components/setup/setup.component';
 import { authGuard } from './guards/auth.guard';
+import { setupGuard } from './guards/setup.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-    { path: 'history', component: HistoryComponent, canActivate: [authGuard] },
-    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: 'setup', component: SetupComponent, canActivate: [authGuard] },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard, setupGuard] },
+    { path: 'history', component: HistoryComponent, canActivate: [authGuard, setupGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard, setupGuard] },
 ];
